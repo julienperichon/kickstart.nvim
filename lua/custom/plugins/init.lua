06ca76c1eff8b97e87ce2ac14e9b98da726c383f
Julien Perichon <julien.perichon@sicara.com>
2023-12-24 14:21:07 +0100

    Re-add Neotree with auto-close on file opening

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,22 +8,32 @@ return {
         event = "InsertEnter",
         opts = {} -- this is equalent to setup({}) function
     },
-    -- {
-    --     "nvim-neo-tree/neo-tree.nvim",
-    --     branch = "v3.x",
-    --     dependencies = {
-    --         "nvim-lua/plenary.nvim",
-    --         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    --         "MunifTanjim/nui.nvim",
-    --         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    --     },
-    --     opts = {
-    --         sources = {
-    --             "filesystem",
-    --             "buffers",
-    --             "git_status",
-    --             "document_symbols",
-    --         }
-    --     }
-    -- }
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        },
+        opts = {
+            sources = {
+                "filesystem",
+                "buffers",
+                "git_status",
+                "document_symbols",
+            },
+            event_handlers = {
+                {
+                    event = "file_opened",
+                    handler = function(file_path)
+                        require("neo-tree.command").execute({ action = "close" })
+                    end
+                },
+
+            }
+
+        }
+    }
 }
