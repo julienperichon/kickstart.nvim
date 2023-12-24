7b1b7cf1ba2522ebd901d7b529d20f29053b602a
Julien Perichon <julien.perichon@sicara.com>
2023-12-24 12:53:40 +0100

    Adding neo-tree as commented plugin as it is pretty heavy to use for my current workflow

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,4 +8,22 @@ return {
         event = "InsertEnter",
         opts = {} -- this is equalent to setup({}) function
     },
+    -- {
+    --     "nvim-neo-tree/neo-tree.nvim",
+    --     branch = "v3.x",
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim",
+    --         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    --         "MunifTanjim/nui.nvim",
+    --         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    --     },
+    --     opts = {
+    --         sources = {
+    --             "filesystem",
+    --             "buffers",
+    --             "git_status",
+    --             "document_symbols",
+    --         }
+    --     }
+    -- }
 }
