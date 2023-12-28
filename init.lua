5178c39728f413e07decf3cd9bbb8fa160e84589
Julien Perichon <julien.perichon@sicara.com>
2023-12-29 00:36:29 +0100

    Add LSP for YAML language

--- init.lua
+++ init.lua
@@ -551,6 +551,7 @@ local servers = {
   -- gopls = {},
   pyright = {},
   taplo = {},
+  yamlls = {},
   -- rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
